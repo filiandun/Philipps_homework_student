27beef9d5db8a3b7c0e85a1734b1ac044ea83b75
filian <filiandun@gmail.com>
2022-11-21 20:10:17 +0300

    complete class "student"

--- student/student.cpp
+++ student/student.cpp
@@ -1,1 +1,29 @@
 #include "student.h"
+#include <iostream>
+
+Student::Student() {
+	name = new char[20];
+	age = 0;
+}
+
+Student::~Student() {
+	delete[]name;
+}
+
+// "SET" FUNCTIONS
+void Student::setName(char* name) {
+	this->name = name;
+}
+
+void Student::setAge(int age) {
+	this->age = age;
+}
+
+// "GET" FUNCTIONS
+char* Student::getName() {
+	return this->name;
+}
+
+int Student::getAge() {
+	return this->age;
+}
