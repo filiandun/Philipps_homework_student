4775776ac3d03e3f6a0febc2039e8a31059117d3
filian <filiandun@gmail.com>
2022-11-21 20:12:06 +0300

    complete main.cpp

--- student/main.cpp
+++ student/main.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
+#include "student.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+    Student* student = new Student;
+
+    // ENTER NAME
+    char* name = new char[20];
+    std::cout << "Введите имя: "; std::cin >> name; std::cout << std::endl;
+    student->setName(name);
+
+    // ENTER AGE
+    int age;
+    std::cout << "Введите возраст: "; std::cin >> age; std::cout << std::endl;
+    student->setAge(age);
+
+    // OUTPUT
+    std::cout << "Имя: " << student->getName(); std::cout << std::endl;
+    std::cout << "Возраст: " << student->getAge();
+
+    delete student; // clear memory
 }
